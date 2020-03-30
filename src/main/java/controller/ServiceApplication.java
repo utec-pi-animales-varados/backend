11a17b5c403d20324d5f3db54c72f8153c290c49
jesubellido <jesubellido@gmail.com>
2020-03-30 10:04:44 -0500

    Adding dependencies #1

---
+++ src/main/java/controller/ServiceApplication.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+@ComponentScan({"controller" })
+@EntityScan("data")
+@EnableJpaRepositories("data.repositories")
+public class ServiceApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
