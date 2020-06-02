647266ea1d7b80592a8f571bc1fcde1cae4cea2c
carlos-reategui <carlos.reategui@utec.edu.pe>
2020-06-01 19:11:58 -0500

    issue #58 resuelto

--- src/main/java/controller/ImageController.java
+++ src/main/java/controller/ImageController.java
@@ -1,6 +1,7 @@
 package controller;
 
-import business.UploadImagenService;
+import business.ImageService;
+import constants.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,27 +9,41 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
-@RequestMapping("/uploadImagen")
-public class UploadImagenController {
+@RequestMapping("/imagen")
+public class ImageController {
     final static String clientUrl = "*";
 
     @Autowired
-    private UploadImagenService uploadImagenService;
+    private ImageService imageService;
 
     @PostMapping()
     @CrossOrigin(origins = clientUrl)
     public ResponseEntity<?> uploadImage(
             @RequestParam("image") MultipartFile file) throws IOException {
 
-        Path path = uploadImagenService.createCouponPost(file);
+        Path path = imageService.createCouponPost(file);
 
         if (path == null) {
             return new ResponseEntity<>("Format not supported", HttpStatus.NOT_FOUND);
         }
 
         return new ResponseEntity<>(path, HttpStatus.OK);
+    }
+
+
+    @GetMapping()
+    @CrossOrigin(origins = clientUrl)
+    public ResponseEntity<?> getImage(
+            @RequestParam("path") String path) throws IOException {
+
+        Path file_path = Paths.get(Constants.IMAGE_PATH, path);
+
+        byte[] img = Files.readAllBytes(file_path);
+        return new ResponseEntity<>(img, HttpStatus.OK);
     }
 }
