ca3b9785e62299c1b6eaf52cc591e599bb072b2f
carlos-reategui <carlos.reategui@utec.edu.pe>
2020-06-22 10:14:01 -0500

    issues #78 y #79 resueltos :white_check_mark:

--- src/main/java/data/repositories/PreguntaRepository.java
+++ src/main/java/data/repositories/PreguntaRepository.java
@@ -3,5 +3,8 @@ package data.repositories;
 import data.entities.Pregunta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PreguntaRepository extends JpaRepository<Pregunta, Long> {
+    public List<Pregunta> findAllByOrderByPrioridadAsc();
 }
